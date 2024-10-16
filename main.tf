0cd7e9e0fabef43f1d1fda12aae97efa37fdbf5a
CloudCMDO <webtisgpay@gmail.com>
2024-10-16 09:21:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "tt-githubdemo-bucket-002"
   project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
