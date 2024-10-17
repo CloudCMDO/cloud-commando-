9a1734996ac2e2b04021a337c977ca6bbacb6867
CloudCMDO <webtisgpay@gmail.com>
2024-10-17 19:19:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
 provider "google" {
-  project = "prefab-faculty-438111-k7"  # Replace with your GCP project ID
-  region  = "us-central1"          # Replace with your preferred region
+  project = "prefab-faculty-438111-k7"
+  region  = "us-central1"
 }
-
 resource "google_storage_bucket" "amitpawar65_bucket" {
-  name     = "amitpawar65"         # The bucket name must be globally unique
-  location = "US"                  # Location where your bucket will be created
-
-  # Optional: Add any bucket configurations you need
-  force_destroy = true             # Set to true if you want to delete all objects when destroying the bucket
+  name     = "amitpawar65"         
+  location = "US"                  
+  force_destroy = true             
 }
-
