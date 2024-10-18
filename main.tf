585eaac8e49b8b75ac62a210dd05b2382e41fcdc
CloudCMDO <webtisgpay@gmail.com>
2024-10-18 08:57:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "google" {
   project = "prefab-faculty-438111-k7"
   region  = "us-central1"
 }
-resource "google_storage_bucket" "Cloud commando" {
-  name     = "Cloud commando"         
+resource "google_storage_bucket" "amitpawar65_bucket" {
+  name     = "amitpawar65"         
   location = "US"                  
   force_destroy = true             
 }
